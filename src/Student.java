4b0c17d09f4d749c44558a24fb535aee362e800f
hazar tatish <117504649+HazarTatish@users.noreply.github.com>
2023-02-28 13:32:37 +0300

    add java code file

---
+++ src/Student.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Student {
+
+
+
+
+
+    public static void main (String[] args)
+    {
+        String studentName;
+        int age;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please Enter Your Name");
+        studentName=input.nextLine();
+        System.out.println("Please Enter Your Age");
+        age=input.nextInt();
+
+
+
+
+    }
+}
